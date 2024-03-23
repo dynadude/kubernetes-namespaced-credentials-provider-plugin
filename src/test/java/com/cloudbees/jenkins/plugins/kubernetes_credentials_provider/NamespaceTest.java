4646576397a6398c4c8477b8d78fd8516e3a89d1
Sagi Gamliel <sagi.gam.11@gmail.com>
2024-03-23 19:43:36 +0000

    NamespaceTest.java: added tests for name validation

--- src/test/java/com/cloudbees/jenkins/plugins/kubernetes_credentials_provider/NamespaceTest.java
+++ src/test/java/com/cloudbees/jenkins/plugins/kubernetes_credentials_provider/NamespaceTest.java
@@ -2,12 +2,15 @@ package com.cloudbees.jenkins.plugins.kubernetes_credentials_provider;
 
 import static org.junit.Assert.*;
 
+import hudson.util.FormValidation;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class NamespaceTest {
+    private Namespace.DescriptorImpl descriptor = new Namespace.DescriptorImpl();
+
     @Test
     public void getName() {
         String nsName = "hello";
@@ -68,5 +71,59 @@ public class NamespaceTest {
         Namespace ns = new Namespace(nsName);
 
         assertEquals(nsName, ns.toString());
+    }
+
+    @Test
+    public void doCheckNameValidNames() {
+        assertDoCheckNameOk("hello");
+        assertDoCheckNameOk("5hello");
+        assertDoCheckNameOk("hello5");
+        assertDoCheckNameOk("5hello5");
+        assertDoCheckNameOk("5-hello-5");
+    }
+
+    private void assertDoCheckNameOk(String name) {
+        assertDoCheckName(FormValidation.ok(), name);
+    }
+
+    @Test
+    public void doCheckNameBlankName() {
+        assertDoCheckName(
+                FormValidation.error(Messages.KubernetesNamespacedCredentialsProvider_MandatoryProperty()), "");
+    }
+
+    @Test
+    public void doCheckNameInvalidCharacters() {
+        assertDoCheckName(
+                FormValidation.error(Messages.KubernetesNamespacedCredentialsProvider_InvalidCharacters()),
+                "._hello#$");
+
+        assertDoCheckName(
+                FormValidation.error(Messages.KubernetesNamespacedCredentialsProvider_InvalidCharacters()), "Hello");
+
+        assertDoCheckName(
+                FormValidation.error(Messages.KubernetesNamespacedCredentialsProvider_InvalidCharacters()), "hellO");
+    }
+
+    @Test
+    public void doCheckNameStartsWithDash() {
+        assertDoCheckName(
+                FormValidation.error(Messages.KubernetesNamespacedCredentialsProvider_StartsWithDash()), "-hello#$");
+    }
+
+    @Test
+    public void doCheckNameEndsWithDash() {
+        assertDoCheckName(
+                FormValidation.error(Messages.KubernetesNamespacedCredentialsProvider_EndsWithDash()), "hello5-");
+
+        assertDoCheckName(
+                FormValidation.error(Messages.KubernetesNamespacedCredentialsProvider_EndsWithDash()), "hello#$-");
+    }
+
+    private void assertDoCheckName(FormValidation expected, String name) {
+        assertEquals(
+                "Namespace name: '" + name + "'",
+                expected.toString(),
+                descriptor.doCheckName(name).toString());
     }
 }
